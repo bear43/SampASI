1472ff2c5bd7581dbca056f2d06f69d352d9ed03
Konstanta <bigwilly4307@yandex.ru>
2019-01-08 01:13:59 +0400

    StopRadio function add

--- Dll1/dllmain.cpp
+++ Dll1/dllmain.cpp
@@ -3,7 +3,9 @@
 
 const char* radioStation = "http://air.radiorecord.ru:8101/rr_320";//Link to a radio station
 DWORD dwBaseSampAddress = NULL;//Base address of samp.dll
-DWORD dwBaseSampAddressOffset = 0x62DA0;//Offset in samp.dll to radio function
+//DWORD dwBaseSampAddressOffset = 0x62DA0;//Offset in samp.dll to radio function SA-MP 0.3.7 R1
+const DWORD dwBaseSampAddressOffsetToPlayFunc = 0x661F0;//Offset in SA-MP 0.3.7 R3
+const DWORD dwBaseSampAddressOffsetToStopFunc = 0x65DF0;//Offset in SA-MP 0.3.7 R3
 const char* one = new char(1);//Just for work. Don't know how to do another way
 
 /*
@@ -25,25 +27,34 @@ void _stdcall startRadioPlay(const char* szURL, DWORD dwUnknownParam1, DWORD dwU
 	}
 }*/
 /* No assembler. Working too. May be not always.*/
-void (_stdcall *pStartRadioPlay)(const char* szURL, DWORD dwUnknownParam1, DWORD dwUnknownParam2, DWORD dwUnknownParam3, const float fVolumeLevel, DWORD dwUnknowParam4);
+void(_stdcall *pStartRadioPlay)(const char* szURL, DWORD dwUnknownParam1, DWORD dwUnknownParam2, DWORD dwUnknownParam3, const float fVolumeLevel, DWORD dwUnknowParam4);
+void(_stdcall* pChannelStop)(char cUnknownOffset);
 
 void check()
 {
 	DWORD addrToCall = NULL;
 	while (true)
 	{
-		if (GetKeyState(VK_F3) < 0)
+		if (GetKeyState(VK_F3) & 0x8000)
 		{
 			if (dwBaseSampAddress == NULL)
 			{
 				dwBaseSampAddress = (DWORD)GetModuleHandle(L"samp.dll");
-				pStartRadioPlay = (void(_stdcall *)(const char*, DWORD, DWORD, DWORD, const float, DWORD))(dwBaseSampAddress + dwBaseSampAddressOffset);
+				pStartRadioPlay = (void(_stdcall *)(const char*, DWORD, DWORD, DWORD, const float, DWORD))(dwBaseSampAddress + dwBaseSampAddressOffsetToPlayFunc);
+				pChannelStop = (void(_stdcall *)(char))(dwBaseSampAddress + dwBaseSampAddressOffsetToStopFunc);
 			}
-			_asm mov ecx, one//SA-MP checks(compares with 1) [ecx] and if ecx is unreadable then fault and crash
-			pStartRadioPlay(radioStation, 0, 0, 0, 50.0f, 0);
+			if (GetKeyState(0x35) & 0x8000)
+			{
+				_asm mov ecx, one//SA-MP checks(compares with 1) [ecx] and if ecx is unreadable then fault and crash
+				pChannelStop(1);
+			
+			}
+			else
+			{
+				_asm mov ecx, one//SA-MP checks(compares with 1) [ecx] and if ecx is unreadable then fault and crash
+				pStartRadioPlay(radioStation, 0, 0, 0, 50.0f, 0);
+			}
 		}
-		else if (GetKeyState(VK_F9) < 0)
-			break;
 		Sleep(15);
 	}
 }
