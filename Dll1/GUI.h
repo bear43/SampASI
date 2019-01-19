b632c11523921dc10b628ba8403eb71618bf52d4
Konstanta <bigwilly4307@yandex.ru>
2019-01-19 17:53:44 +0400

    Attached ImGUI

--- Dll1/GUI.h
+++ Dll1/GUI.h
@@ -1,6 +1,20 @@
 #pragma once
+#include "imgui/imgui.h"
+#include "imgui/examples/imgui_impl_dx9.h"
+#include "imgui/examples/imgui_impl_win32.h"
+#include <d3d9.h>
+#include <Windows.h>
+
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "Xinput.lib")
+#pragma comment(lib, "Xinput9_1_0.lib")
+
+#define GTA_SA_D3D9_OFFSET 0xC97C28
+
 class GUI
 {
+private:
+
 public:
 	GUI();
 	~GUI();
--- Dll1/dllmain.cpp
+++ Dll1/dllmain.cpp
@@ -2,9 +2,59 @@
 #include "File\FileOutput.h"
 #include "File\FileInput.h"
 #include "CRadio\CRadio.h"
+#include "GUI.h"
 
 constexpr auto DELAY_TIME = 30;
 
+IDirect3DDevice9 *pDevice = nullptr;
+bool bInited = false;
+ImGuiIO io;
+char* originalBytes = nullptr;
+bool bShow = false;
+
+typedef HRESULT(_stdcall* oEndScene)(LPDIRECT3DDEVICE9 pDevice);
+
+oEndScene originalEndScene = nullptr;
+
+void patchPointer(DWORD destinationAddress, DWORD newValue)
+{
+	const char arr[] = { newValue & 0xFF, (newValue >> 8) & 0xFF, (newValue >> 16) & 0xFF, newValue >> 24 };
+	patchBytes(destinationAddress, arr, 4);
+}
+
+HRESULT _stdcall imguil(LPDIRECT3DDEVICE9 pDevice)
+{
+	if (!bInited)
+	{
+		ImGui::CreateContext();
+		io = ImGui::GetIO();
+		ImGui::StyleColorsClassic();
+		ImGui_ImplWin32_Init(FindWindowA(NULL, "GTA:SA:MP"));
+		ImGui_ImplDX9_Init(pDevice);
+		bInited = true;
+	}
+	ImGui_ImplDX9_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+	if (bShow)
+	{
+		
+		ImGui::ShowDemoWindow();
+	}
+	ImGui::EndFrame();
+	ImGui::Render();
+	ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+	return originalEndScene(pDevice);
+
+}
+
+void hook()
+{
+	DWORD **pPointer = (DWORD**)GTA_SA_D3D9_OFFSET;
+	DWORD ppPointer = **pPointer;
+	originalEndScene = (oEndScene)(*(DWORD*)(ppPointer + 0xA8));
+	patchPointer(ppPointer + 0xA8, (DWORD)&imguil);
+}
 
 void check()
 {
@@ -17,6 +67,7 @@ void check()
 	CSamp::patchConnectDelayTimer();//Patches delay
 	CRadio::init();
 	CRadio::loadAllInstances();
+	hook();
 	while (true)
 	{
 		if (GetKeyState(VK_F3) & 0x8000)
@@ -24,6 +75,7 @@ void check()
 			if (GetKeyState(0x35) & 0x8000)
 			{
 				CRadio::stop();
+				bShow = !bShow;
 			}
 			else
 			{
@@ -33,10 +85,13 @@ void check()
 		if (GetKeyState(VK_F12))
 		{
 			CSamp::sendMessage("Exiting from plugin");
-			return;
+			break;
 		}
 		Sleep(DELAY_TIME);
 	}
+	ImGui_ImplDX9_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
